c68ae165c58061f446111ebca00548c035f22828
iamLolade <adeniyiotemade@gmail.com>
2022-04-21 14:44:57 +0100

    Updated bookContext to adopt reducer

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,23 +1,13 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
 import { v1 as uuidv1 } from "uuid";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: "JavaScript for Dummies", author: "John Doe", id: 1},
-        {title: "JavaScript Fundamentals", author: "Jane Doe", id: 2}
-    ]);
+    const [books, dispatch] = useReducer([]);
 
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuidv1() }])
-    }
-
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id ))
-    }
     return (
-        <BookContext.Provider value={{ books, addBook, removeBook }}>
+        <BookContext.Provider value={{ books, dispatch }}>
             {props.children}
         </BookContext.Provider>
     )
